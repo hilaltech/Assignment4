873d479d351902c733c341935fa1ce10ab85a133
HilalAhmad <hilal.mohammadzai111@gmail.com>
2021-06-29 21:14:56 +0430

    Java Main file added by Hilal

---
+++ main.java
@@ -0,0 +1,13 @@
+// Hello World
+
+import java.util.Scanner;
+public class main{
+
+        // The main method
+        public static void main(String [] args){
+        
+       System.out.println("Hello Java Developers");
+
+
+}
+}
