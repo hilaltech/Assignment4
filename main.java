7bfbff91b56a6c1e3580b8063f8fa5c4591d4dee
HilalAhmad <hilal.mohammadzai111@gmail.com>
2021-06-30 08:33:16 +0430

    Python file add by Hilal

--- main.java
+++ main.java
@@ -1,9 +1,8 @@
-// Hello World
 
-import java.util.Scanner;
+
 public class main{
 
-        // The main method
+        
         public static void main(String [] args){
         
        System.out.println("Hello Java Developers");
